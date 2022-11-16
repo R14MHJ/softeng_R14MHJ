33c06751f44c8e7c7f6c566129691d162179f1ac
r14mhj <r14mhj@SZA0030904>
2022-11-16 13:22:19 +0100

    Add project files.

---
+++ Gyak09_R14MHJ/Controllers/JokeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Gyak09_R14MHJ.Controllers
+{
+    [Route("api/jokes")]
+    [ApiController]
+    public class JokeController : ControllerBase
+    {
+        // GET: api/jokes
+        [HttpGet]
+        public IActionResult Get()
+        {
+            JokeModels.FunnyDatabaseContext Context = new JokeModels.FunnyDatabaseContext();
+            return Ok(Context.Jokes.ToList());
+        }
+
+        // GET api/jokes/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            JokeModels.FunnyDatabaseContext Context = new JokeModels.FunnyDatabaseContext();
+            var kervicc = (from x in Context.Jokes
+                          where x.JokeSk == id
+                          select x).FirstOrDefault();
+            return Ok(kervicc);
+        }
+
+        // POST api/jokes
+        [HttpPost]
+        public void Post([FromBody] JokeModels.Joke újvicc)
+        {
+            JokeModels.FunnyDatabaseContext Context = new JokeModels.FunnyDatabaseContext();
+            Context.Jokes.Add(újvicc);
+            Context.SaveChanges();
+        }
+
+        // PUT api/<JokeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/jokes/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            JokeModels.FunnyDatabaseContext context = new JokeModels.FunnyDatabaseContext();
+            var todel = (from x in context.Jokes
+                         where id == x.JokeSk
+                         select x).FirstOrDefault();
+            context.Jokes.Remove(todel);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Gyak09_R14MHJ/JokeModels/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gyak09_R14MHJ.JokeModels;
+
+public partial class Joke
+{
+    public int JokeSk { get; set; }
+
+    public string? JokeText { get; set; }
+
+    public int UpVotes { get; set; }
+
+    public int DownVotes { get; set; }
+}
