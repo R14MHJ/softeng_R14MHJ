006b289ced62b5a5caef86b01b3359dc3ebff4e2
Gábor Márk Könczey <gabor.konczey@stud.uni-corvinus.hu>
2022-11-23 13:21:14 +0100

    Gyak

---
+++ Gyak09_R14MHJ/wwwroot/geek.js
@@ -0,0 +1,25 @@
+﻿var viccek;
+var kerdesek;
+
+function download(d) {
+    let ide = document.getElementById("ide");
+    console.log("siker")
+    console.log(' ${d.length} vicc erkezett')
+    console.log(d)
+    viccek = d;
+    for (var i = 0; i < d.length; i++) {
+        let elem = document.createElement("div")
+        elem.innerHTML = d[i].text
+        ide.appendChild(elem)
+    }
+    for (var i = 0; i < d.length; i++) {
+        kerdesek[i] = d[i].text
+    }
+}
+
+window.onload = function () {
+    fetch('jokes.json')
+        .then(response => response.json())
+        .then(data => download(data));
+
+}
---
+++ Gyak09_R14MHJ/wwwroot/jsgyak.js
@@ -0,0 +1,22 @@
+﻿console.log("minta")
+/////for (var i = 0; i < 10; i++) {
+/////   let sor = document.createElement("div")   
+/////
+var factor = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;  
+}
+var factorR = (n) => {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    else {
+        return n*factor(n-1)
+    }
+}
+for (var i = 0; i < 10; i++) {
+    console.log(`${i}:${factorR(i)}`)
+}
